a85a0fdd18de997d9f23d8871f863afb0e30d9fd
jwjungwoo <140131247+jwjungwoo@users.noreply.github.com>
2025-04-30 15:47:53 +0900

    DP-40 <testing to stop df>
    문제점: df가 한번 시작하면 reset 해도 계속 작동중

--- DF_Practice/DFPLAYER_MINI.c
+++ DF_Practice/DFPLAYER_MINI.c
@@ -1,4 +1,6 @@
 #include  "DFPLAYER_MINI.h"
+#include "string.h"
+#include "hw_vcom.h"
 #include <stdio.h>
 
 extern UART_HandleTypeDef huart1;
@@ -6,7 +8,7 @@ extern UART_HandleTypeDef huart1;
 void Send_Cmd (uint8_t cmd, uint8_t Parameter1, uint8_t Parameter2)
 {
 	uint16_t Checksum = Version + Cmd_Len + cmd + Feedback + Parameter1 + Parameter2;
-    Checksum = 0 - Checksum;
+  Checksum = 0 - Checksum;
 
 	uint8_t CmdSequence[10] = { Start_Byte, Version, Cmd_Len, cmd, Feedback, Parameter1, Parameter2, (Checksum>>8)&0x00ff, (Checksum&0x00ff), End_Byte};
 
@@ -27,8 +29,16 @@ void Sound_Track(uint8_t nums){
 
 void DF_Init (uint8_t volume)
 {
+	Printf("DF_Init \r\n");
+	
 	Send_Cmd(0x3F, 0x00, Source); // initialize parameters
-	HAL_Delay(2000);
-	Send_Cmd(0x06, 0x00, volume);
-	HAL_Delay(2000);
+	Printf("Send Cmd that initialize \r\n");
+	HAL_Delay(200);
+	
+	Send_Cmd(0x06, 0x00, volume); // initialize volume
+	Printf("Send Cmd that change volume \r\n");
+	HAL_Delay(500);
+	
+	Send_Cmd(0x0D, 0x00, 0); // start track 
+	Printf("Start track \r\n");
 }
