4823f90179ce21cf03613cc457511ce4e8bfc72c
kimseojin <gg4ever1724@gmail.com>
2025-05-02 12:23:11 +0900

    DP-60 <1차통합>

--- stm32_sleep/can_L0/Core/Src/main.c
+++ stm32_sleep/can_L0/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -51,8 +51,8 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-extern UART_HandleTypeDef huart2; // USART1
-extern UART_HandleTypeDef huart4; // USART2
+extern UART_HandleTypeDef huart2; // USART2
+extern UART_HandleTypeDef huart4; // USART4
 uCAN_MSG rxMessage;
 /* USER CODE END PV */
 
@@ -67,25 +67,25 @@ void SystemClock_Config(void);
 volatile uint8_t can_rx_flag = 0;
 void do_wake_up(void)
 {
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5); // LED ON
-		HAL_Delay(100);
-		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5); // LED ON
-		HAL_Delay(100);
+  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5); // LED ON
+  HAL_Delay(100);
+  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5); // LED ON
+  HAL_Delay(100);
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-	 if (GPIO_Pin == GPIO_PIN_9)  // PB9 INT
-    {
-				can_rx_flag = 1;
-    }
+  if (GPIO_Pin == CAN_INT_Pin) // PB9 INT
+  {
+    can_rx_flag = 1;
+  }
 }
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
@@ -97,7 +97,7 @@ int main(void)
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
+  HAL_Delay(1000);
   /* USER CODE BEGIN Init */
 
   /* USER CODE END Init */
@@ -115,57 +115,72 @@ int main(void)
   MX_USART2_UART_Init();
   MX_USART4_UART_Init();
   /* USER CODE BEGIN 2 */
-	HW_VCOM_Init(&huart2);
-	DF_Init(2);
-  CANSPI_Initialize();	
-	lcd_init();
-	MCP2515_BitModify(MCP2515_CANINTE, 0x03, 0x03);
-  
-	DF_Resume();
+  HW_VCOM_Init(&huart2);
+  DF_Init(2);
+  CANSPI_Initialize();
+  lcd_init();
+  MCP2515_BitModify(MCP2515_CANINTE, 0x03, 0x03);
+  MCP2515_EnableInterrupts();
+
+  DF_Resume();
   Printf("DF_Init finished\r\n");
   HAL_Delay(6000);
-    
+
   DF_Pause(); // pause
   Printf("DF_Pause \r\n");
   HAL_Delay(6000);
-    
+
   DF_Resume();
   Printf("DF_Resume \r\n");
   HAL_Delay(6000);
-      
+
   Sound_Track(2); // play 2nd song
   Printf("Sound_Track 2 \r\n");
-	
-	/* USER CODE END 2 */
+
+  HAL_SuspendTick();
+  HAL_PWR_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFI);
+
+  /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-		
-		lcd_clear(); // LCD ��� ���� �ʱ�ȭ �Լ�
+    if (can_rx_flag)
+    {
+      HAL_ResumeTick();
+      can_rx_flag = 0;
+      while (CANSPI_messagesInBuffer() > 0)
+      {
+        if (CANSPI_Receive(&rxMessage))
+        {
+          // Set song number from Raspberry pi CAN messege !!!!!!!!!!!!
+          // STM32 check
+          if (rxMessage.frame.id)
+          {
+            do_wake_up();
+          }
 
-    char buf[32];  
-    sprintf(buf, "Hello World 1!"); 
-    lcd_print(buf);  // ����� ������ ���ڿ��� ����� LCD���� ����
-
-    lcd_goto(1,0); // LCD Ŀ�� ��ġ �̵� �Լ�
-    lcd_print("Hello World 2!");
-		Printf("hello world!\r\n");
-    HAL_Delay(100);
-    
-  }
+          // Raspberry check
+          //					else if(){
+          //
+          //					}
+        }
+      }
+      HAL_SuspendTick();
+      HAL_PWR_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFI);
+    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	
+  }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -173,12 +188,12 @@ void SystemClock_Config(void)
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -192,9 +207,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -217,9 +231,9 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -231,14 +245,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
